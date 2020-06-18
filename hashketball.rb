22d6b4ee3959c48a1fb7a55e2233dd5777f302b5
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-06-18 22:25:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,25 +2,25 @@
 def game_hash
   {
    home:{
-     team_name: "Brooklyn Nets"
-     colors: ["Black", "White"]
+     team_name: "Brooklyn Nets",
+     colors: ["Black", "White"],
      players: [
        {
          player_name: "Alan Anderson",
-         number: 0 
-         shoe: 16
-         points: 22
-         rebounds: 12
-         assists: 12
-         steals: 3
-         blocks: 1
+         number: 0, 
+         shoe: 16,
+         points: 22,
+         rebounds: 12,
+         assists: 12,
+         steals: 3,
+         blocks: 1,
          slam_dunks: 1
        }
        ]
    }
    away:{
-     team_name: "Charlotte Hornets"
-     colors: ["Turquoise", "Purple"]
+     team_name: "Charlotte Hornets",
+     colors: ["Turquoise", "Purple"],
      players: [
 
        ]

47b5a40cd91b8a4c957cb05b84941f976542830d
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-06-18 22:11:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,19 @@
 # Write your code here!
 def game_hash
   {
-    
+   home:{
+     team_name:{
+       
+     }
+     colors:{
+       
+     }
+     players:{
+       
+     }
+   }
+   away:{
+     
+   }
   }
 end 

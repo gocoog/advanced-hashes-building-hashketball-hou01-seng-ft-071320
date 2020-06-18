8aac805bd8056de9d80b72469e363e6fca9426e6
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-06-18 22:24:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,8 +3,8 @@ def game_hash
   {
    home:{
      team_name: "Brooklyn Nets"
-     colors:["Black", "White"]
-     players:[
+     colors: ["Black", "White"]
+     players: [
        {
          player_name: "Alan Anderson",
          number: 0 
